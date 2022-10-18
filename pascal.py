0efb74a2a73882a98dfa3901b9154da8e791936d
benrmartins <benrmartins@gmail.com>
2022-10-18 11:03:31 -0400

    project 3

--- pascal.py
+++ pascal.py
@@ -3,28 +3,28 @@ import stdio
 import sys
 
 # Accept n (int) as command-line argument.
-...
+n = int(sys.argv[1])
 
 # Setup a 2D ragged list a of integers. The list must have n + 1 rows, with the ith (0 <= i
 # <= n) row a[i] having i + 1 elements, each initialized to 1. For example, if n = 3, a should be
 # initialized to [[1], [1, 1], [1, 1, 1], [1, 1, 1, 1]].
-a = ...
-for i in range(...):
-    ...
+a = stdarray.create2D(n + 1, none)
+for i in range(n + 1):
+    a[i] = i + 1
 
 # Fill the ragged list a using the formula for Pascal's triangle
 #     a[i][j] = a[i - 1][j - 1] + a[i - 1][j]
 # where 0 <= i <= n and 1 <= j < i.
-for i in range(...):
-    for j in range(...):
-        ...
+for i in range(0, i):
+    for j in range(j, i):
+        a[i][j] = a[i-1][j-1] + a[i-1][j]
 
 # Write a to standard output.
-for i in range(...):
-    for j in range(...):
-        if ...:
+for i in range(i, n):
+    for j in range(j, n):
+        if j != n:
             # If j is not the last column, write a[i][j] with a space after.
-            ...
+            a[i][j] = " "
         else:
             # Otherwise, write the element with a newline after.
-            ...
+            a[i][j] ="\n"
--- playwaltz.py
+++ playwaltz.py
@@ -2,4 +2,18 @@ import stdaudio
 import stdio
 import sys
 
-...
+waltzMeasure = stdio.readAllInts()
+
+if len(waltzMeasure) != 32:
+    sys.exit()
+for minuetMeasure in waltzMeasure[0, 16]:
+    if(minuetMeasure <= 1 or minuetMeasure >= 76):
+        sys.exit()
+
+# same
+
+for minuetMeasure in waltzMeasure[0, 16]:
+    fileName = 'data/M' + str(minuetMeasure)
+    stdaudio.playFile(fileName);
+
+# 16-32
