e4d73f0ff345f004e1079d40cb702b7364af7598
benrmartins <benrmartins@gmail.com>
2022-10-19 11:32:12 -0400

    project 3

--- generatewaltz.py
+++ generatewaltz.py
@@ -3,25 +3,17 @@ import stdrandom
 import stdio
 
 minuetMeasure = stdarray.create2D(11, 16, 0)
-trioMeasure = stdarray.create2D(6, 16, 0)
 
 for i in range(11):
-    for j in range(16):
-        minuetMeasure[i][j] = stdarray.readInt()
+    for j in range(15):
+        minuetMeasure[i][j] = stdio.readInt()
         i = stdrandom.uniformInt(1, 7) + stdrandom.uniformInt(1, 7)
         minuetMeasure = minuetMeasure[i][j]
         stdio.writeln(str(minuetMeasure) + " ")
 
 for i in range(6):
-    for j in range(16):
-        trioMeasure[i][j] = stdarray.readInt()
+    for j in range(15):
+        trioMeasure[i][j] = stdio.readInt()
         i = stdrandom.uniformInt(1, 7) + stdrandom.uniformInt(1, 7)
         trioMeasure = trioMeasure[i][j]
         stdio.writeln(str(trioMeasure) + " ")
-
-
-
-
-
-
-
--- pascal.py
+++ pascal.py
@@ -8,23 +8,23 @@ n = int(sys.argv[1])
 # Setup a 2D ragged list a of integers. The list must have n + 1 rows, with the ith (0 <= i
 # <= n) row a[i] having i + 1 elements, each initialized to 1. For example, if n = 3, a should be
 # initialized to [[1], [1, 1], [1, 1, 1], [1, 1, 1, 1]].
-a = stdarray.create2D(n + 1, none)
+a = stdarray.create1D(n + 1, 1)
 for i in range(n + 1):
-    a[i] = i + 1
+    a[i] = stdarray.create1D(i + 1, 1)
 
 # Fill the ragged list a using the formula for Pascal's triangle
 #     a[i][j] = a[i - 1][j - 1] + a[i - 1][j]
 # where 0 <= i <= n and 1 <= j < i.
-for i in range(0, i):
-    for j in range(j, i):
+for i in range(n + 1):
+    for j in range(1, i):
         a[i][j] = a[i-1][j-1] + a[i-1][j]
 
 # Write a to standard output.
-for i in range(i, n):
-    for j in range(j, n):
-        if j != n:
+for i in range(n+1):
+    for j in range(len(a[i])):
+        if j < i:
             # If j is not the last column, write a[i][j] with a space after.
-            a[i][j] = " "
+            stdio.write(str(a[i][j]) + " ")
         else:
             # Otherwise, write the element with a newline after.
-            a[i][j] ="\n"
+            stdio.writeln(str(a[i][j]))
