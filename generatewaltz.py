108c2ebe4bc1c2e6617cfb355a48a03a8a1b42ee
benrmartins <benrmartins@gmail.com>
2022-10-22 22:03:17 -0400

    finished

--- generatewaltz.py
+++ generatewaltz.py
@@ -1,19 +1,42 @@
+from typing import Any
+
 import stdarray
 import stdrandom
 import stdio
 
-minuetMeasure = stdarray.create2D(11, 16, 0)
+# Created a 2 2 D arrays, 11 by 16 and 6 by 16 with 0s inside it
+minMeasure = stdarray.create2D(11, 16, 0)
 
+trio = stdarray.create2D(6, 16, 0)
+
+# looped through the 11 by 16 array
 for i in range(11):
-    for j in range(15):
-        minuetMeasure[i][j] = stdio.readInt()
-        i = stdrandom.uniformInt(1, 7) + stdrandom.uniformInt(1, 7)
-        minuetMeasure = minuetMeasure[i][j]
-        stdio.writeln(str(minuetMeasure) + " ")
+    for j in range(16):
+        # replaced each 0 in the 2d array to a number in the file
+        minMeasure[i][j] = stdio.readInt()
 
+# looped through the columns of the array of numbers
+for j in range(16):
+    # randomly from a roll, chose a two rows from the array of numbers and added them together
+    i = (stdrandom.uniformInt(1, 7) - 1) + (stdrandom.uniformInt(1, 7) - 1)
+    # stored the exact value of the row and column into a variable
+    minsMeasure = minMeasure[i][j]
+    # displayed the numbers onto the terminal
+    stdio.write(str(minsMeasure) + " ")
+
+# looped through the 6 by 16 array
 for i in range(6):
-    for j in range(15):
-        trioMeasure[i][j] = stdio.readInt()
-        i = stdrandom.uniformInt(1, 7) + stdrandom.uniformInt(1, 7)
-        trioMeasure = trioMeasure[i][j]
-        stdio.writeln(str(trioMeasure) + " ")
+    for j in range(16):
+        # replaced each 0 in the 2d array to a number in the file
+        trio[i][j] = stdio.readInt()
+
+# looped through the columns of the array of numbers
+for j in range(16):
+    # randomly from a roll, chose a row from the array of numbers
+    i = stdrandom.uniformInt(1, 7) - 1
+    # stored the exact value of the row and column into a variable
+    trios = trio[i][j]
+    # displayed the numbers onto the terminal
+    stdio.write(str(trios) + " ")
+
+stdio.writeln()
--- playwaltz.py
+++ playwaltz.py
@@ -2,18 +2,35 @@ import stdaudio
 import stdio
 import sys
 
-waltzMeasure = stdio.readAllInts()
+# stored the numbers the file into a variable
+waltz = stdio.readAllInts()
 
-if len(waltzMeasure) != 32:
-    sys.exit()
-for minuetMeasure in waltzMeasure[0, 16]:
-    if(minuetMeasure <= 1 or minuetMeasure >= 76):
-        sys.exit()
+# exited if the measure is not 32
+if len(waltz) != 32:
+    sys.exit("A waltz must contain exactly 32 measures")
 
-# same
+# looped through the first 16 minutes
+for min in waltz[0:16]:
+    # exited if the number was greater 176 and less than 1
+    if min > 176 or min < 1:
+        sys.exit("A minuet measure must be from [1, 176]")
 
-for minuetMeasure in waltzMeasure[0, 16]:
-    fileName = 'data/M' + str(minuetMeasure)
-    stdaudio.playFile(fileName);
+# checked if the number was in the first 16 min
+if min in waltz[1:16]:
+    # stored the filename along with the number of the sound
+    filename = "data/M" + str(min)
+    # played the sound using from the file name
+    stdaudio.playFile(filename)
 
-# 16-32
+# looped through the last 16 minutes
+for trio in waltz[16:32]:
+    # exited if the number was greater 96 and less than 1
+    if trio > 96 or trio < 1:
+        sys.exit("A trio measure must be from [1, 96]")
+
+# checked if the number was in the last 16 min
+if trio in waltz[16:32]:
+    # stored the filename along with the number of the sound
+    filename = "data/T" + str(trio)
+    # played the sound using from the file name
+    stdaudio.playFile(filename)
